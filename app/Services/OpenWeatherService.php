2c869d5a23f91bbbb99abf24260dee80f14489d0
Edvardas Baltrunas <edvardas@approq.com>
2024-11-27 21:49:17 +0200

    Basic OpenWeatherService, OpenWeather package installed, key added

---
+++ app/Services/OpenWeatherService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+class OpenWeatherService
+{
+    protected function getCityCoordinates()
+    {
+
+    }
+
+    protected function getWeatherConditions()
+    {
+
+    }
+}
